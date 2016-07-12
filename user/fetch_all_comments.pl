dc23d493f4a8f32bb27a66a7abd09dc0e72ebd5d
Dee Newcum <dee.newcum@gmail.com>
2016-07-12 15:34:15 -0500

    [fetch_all_comments] refactor, in preparation for an upcoming change

--- user/fetch_all_comments.pl
+++ user/fetch_all_comments.pl
@@ -22,31 +22,48 @@ $username =~ s#^https?://([^/]+\.)?reddit\.com/user/##;
 
 my $after = undef;
 
-my @data_children;        # all of the entries under $json->{data}{children}  on JSON responses
 
 my $json_obj = JSON::XS->new->utf8;
 
 my $filename_out = "$username.json";
+
+# all of the entries under $json->{data}{children}  on JSON responses
+my @data_children = fetch_1000("http://www.reddit.com/user/$username/.json");
+
 my $json_out = {data => {children => \@data_children}};
 
-while (1) {
-    my $count = scalar(@data_children);
-    printf "%2d\n",     $count;
-    my $url = "http://www.reddit.com/user/$username/.json";
-    $url .= "?count=$count&after=$after"    if defined($after);
-    my $response = get $url
-            or last;
-    my $json = $json_obj->decode($response);
-    last unless $json->{data}{children};
-    push(@data_children, @{$json->{data}{children}});
+open my $fout, '>', $filename_out   or die $!;
+print $fout $json_obj->encode($json_out);
+close $fout;
 
-    open my $fout, '>', $filename_out   or die $!;
-    print $fout $json_obj->encode($json_out);
-    close $fout;
 
-    $after = $json->{data}{after}   or last;
+
+# fetch as many posts as possible, clicking "next" as many times as is necessary
+sub fetch_1000 {
+    my ($url) = @_;
+
+    # prepare the URL for appending of the count
+    if ($url =~ /\?/) {
+        $url .= "&";
+    } else {
+        $url .= "?";
+    }
+
+    my @data_children;
+
+    while (1) {
+        my $count = scalar(@data_children);
+        printf "%2d\n",     $count;
+        my $url = "http://www.reddit.com/user/$username/.json";
+        $url .= "?count=$count&after=$after"    if defined($after);
+        my $response = get $url
+                or last;
+        my $json = $json_obj->decode($response);
+        last unless $json->{data}{children};
+        push(@data_children, @{$json->{data}{children}});
+
+        $after = $json->{data}{after}   or last;
+    }
+
+    return @data_children;
 }
-
-
-
-
